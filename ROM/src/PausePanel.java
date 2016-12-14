096ea6ad70953047b7fe0102a028d5a4c3acef6f
efeUlasAkay <efeulasakayseyitoglu@gmail.com>
2016-12-14 22:07:06 +0200

    Pause Panel Updated

--- ROM/src/PausePanel.java
+++ ROM/src/PausePanel.java
@@ -1,10 +1,22 @@
-public class PausePanel {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	private int listOfButton;
+/**
+ * Created by Seyitoglu on 14.12.2016.
+ */
+public class PausePanel extends BackgroundImagePanel{
 
-	public void selectButton() {
-		// TODO - implement PausePanel.selectButton
-		throw new UnsupportedOperationException();
-	}
+    private JButton continueButton;
+
+    public PausePanel() {
+        continueButton = new JButton("Continue");
+        continueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*New Panel will be instantiated*/
+            }
+        });
+    }
 
 }
