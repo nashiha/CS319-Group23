3229631b34a07fe306d0c37d3933103ec1c47875
efeUlasAkay <efeulasakayseyitoglu@gmail.com>
2016-12-14 16:55:31 +0200

    Class Added to provide background via Images

---
+++ ROM/src/BackgroundImagePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Seyitoglu on 12.12.2016.
+ */
+public class BackgroundImagePanel extends JPanel {
+
+    /*Sets the background of the image to a background provided*/
+    private Image myImage;
+
+
+}
