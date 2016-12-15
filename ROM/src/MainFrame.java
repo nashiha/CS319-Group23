a06ca4148d9d7d44d9d1752d78865f19d4d10bc8
nashiha <nashiha.ahmed@yahoo.com>
2016-12-15 16:28:54 +0300

    Images folder, MainFrame, MainPanel added

--- ROM/src/MainFrame.java
+++ ROM/src/MainFrame.java
@@ -1,21 +1,28 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.JPanel;
+
+/**
+ * Created by Shiha on 12/14/2016.
+ */
 public class MainFrame {
-
-	private int frame;
-	private int activePanel;
-	private int panels;
-	private int color;
-	private int layout;
-	private int dimension;
-	private int scrollbar;
-
-	public void updateActivePanel() {
-		// TODO - implement MainFrame.updateActivePanel
-		throw new UnsupportedOperationException();
+	public JFrame frame;
+	public MainMenuPanel panel;
+	public void createFrame() {
+		frame = new JFrame( "Reedemers of the Monarchy");
+		frame.setDefaultCloseOperation( frame.EXIT_ON_CLOSE);
+		frame.setMinimumSize( new Dimension(1016,602));
+		panel = new MainMenuPanel();
+		frame.add( panel);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible( true);
 	}
-
-	public void changeActivePanel() {
-		// TODO - implement MainFrame.changeActivePanel
-		throw new UnsupportedOperationException();
+	public static void main(String[] args )
+	{
+		MainFrame frame;
+		frame = new MainFrame();
+		frame.createFrame();
+		System.out.println( "Hello.");
 	}
-
 }
--- ROM/src/MainMenuPanel.java
+++ ROM/src/MainMenuPanel.java
@@ -1,15 +1,85 @@
-public class MainMenuPanel {
+import java.awt.*;
+import javax.swing.*;
 
-	private int panel;
-	private int backgroundimage;
-	private int backgroundcolor;
-	private int layout;
-	private int dimension;
-	private int listOfButton;
+/**
+ * Created by Shiha on 12/14/2016.
+ */
+public class MainMenuPanel extends JPanel {
+	Image background;
+	private JButton button1;
+	private JButton settings;
+	private JButton tutorial;
+	private JButton hscores;
+	private JButton credits;
+	private JButton quit;
 
-	public void selectButton() {
-		// TODO - implement MainMenuPanel.selectButton
-		throw new UnsupportedOperationException();
+	public MainMenuPanel() {
+		setLayout( new BoxLayout(this, BoxLayout.Y_AXIS));
+		background = new ImageIcon("\\images\\Menu_main_no_buttons.png").getImage();
+
+		//play
+		button1 = new JButton();
+		button1.setOpaque(false);
+		//button1.setBorderPainted(false);
+		button1.setContentAreaFilled(false);
+		button1.setIcon(new ImageIcon("\\images\\play.png"));
+		button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		//settings
+		settings = new JButton();
+		settings.setIcon(new ImageIcon("\\images\\settings.png"));
+		settings.setOpaque(false);
+		settings.setRolloverIcon(new ImageIcon("\\images\\tutorial.png"));
+		settings.setPressedIcon(new ImageIcon("\\images\\tutorial.png"));
+		//settings.setBorderPainted(false);
+		settings.setContentAreaFilled(false);
+		settings.setAlignmentX(Component.CENTER_ALIGNMENT);
+		//tutorial
+		tutorial = new JButton();
+		tutorial.setIcon(new ImageIcon("\\images\\tutorial.png"));
+		tutorial.setOpaque(false);
+		//tutorial.setBorderPainted(false);
+		tutorial.setContentAreaFilled(false);
+		tutorial.setAlignmentX(Component.CENTER_ALIGNMENT);
+		//highscores
+		hscores = new JButton();
+		hscores.setIcon(new ImageIcon("\\images\\hscores.png"));
+		hscores.setOpaque(false);
+		hscores.setBorderPainted(false);
+		hscores.setContentAreaFilled(false);
+		hscores.setAlignmentX(Component.CENTER_ALIGNMENT);
+		//credits
+		credits = new JButton();
+		credits.setIcon(new ImageIcon("\\images\\credits.png"));
+		credits.setOpaque(false);
+		credits.setBorderPainted(false);
+		credits.setContentAreaFilled(false);
+		credits.setAlignmentX(Component.CENTER_ALIGNMENT);
+		//quit
+		quit = new JButton();
+		quit.setIcon(new ImageIcon("\\images\\quit.png"));
+		quit.setOpaque(false);
+		quit.setBorderPainted(false);
+		quit.setContentAreaFilled(false);
+		quit.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		//add the buttons
+		add(Box.createRigidArea(new Dimension(0,160)));
+		add( button1);
+		add(Box.createRigidArea(new Dimension(0,5)));
+		add( settings);
+		add(Box.createRigidArea(new Dimension(0,5)));
+		add( tutorial);
+		add(Box.createRigidArea(new Dimension(0,5)));
+		add( hscores);
+		add(Box.createRigidArea(new Dimension(0,5)));
+		add( credits);
+		add(Box.createRigidArea(new Dimension(0,5)));
+		add( quit);
 	}
 
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(background, 0, 0, null);
+	}
 }
