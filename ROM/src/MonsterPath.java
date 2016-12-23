372be8d04ddab1137b61f9593cab0785c1a5e246
efeUlasAkay <efeulasakayseyitoglu@gmail.com>
2016-12-24 00:20:05 +0200

    Monsters are walking when this panel is instantiated

--- ROM/src/MonsterPath.java
+++ ROM/src/MonsterPath.java
@@ -6,40 +6,85 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+import java.util.*;
+/**
+ * Created by Seyitoglu on 24.12.2016.
+ */
 
 public class MonsterPath extends JPanel implements ActionListener {
     Image game_background;
     private Timer timer;
-    private  MonsterPanel p;
+    private  MonsterPanel monsterPanel;
     private Monster monster;
+    private int pathCheck;
+    private TimerTask task;
+    private int currentCheck = 0;
 
 
     public MonsterPath() {
 
         game_background = new ImageIcon(this.getClass().getResource("images/game_background.png")).getImage();
-        p = new MonsterPanel();
-        monster = p.getMonster();
 
-        timer = new Timer(10000,this);
-        timer.start();
-        //repaint();
-        //	add(innerPanel);
+        pathCheck = 0;
+
+        monsterPanel = new MonsterPanel();
+        monster = monsterPanel.getMonster();
+
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                if(currentCheck < 135)
+                     monster.moveRight(1);
+                else if(currentCheck < 355 )
+                    monster.moveDown(1);
+                else if(currentCheck < 650)
+                    monster.moveRight(1);
+                else if(currentCheck < 970)
+                    monster.moveUp(1);
+                else if (currentCheck < 1250)
+                    monster.moveRight(1);
+                else if (currentCheck < 1610)
+                    monster.moveDown(1);
+                else if(currentCheck > 1700)
+                    monster.moveRight(1);
+                else
+                    JOptionPane.showMessageDialog(null,"Monster Reached the castle!!");
+                currentCheck++;
+                repaint();
+            }
+        };
+
+
+
     }
+
+    public void move() {
+        timer.scheduleAtFixedRate(task,100,100);
+    }
+
+
+
 
     @Override
     public void paintComponent(Graphics g) {
         g.drawImage(game_background, 0, 0, null);
 
-        g.drawImage(p.getImage(), monster.getXloc(), monster.getYLoc(), null);
+        g.drawImage(monsterPanel.returnMonsterImage(), monster.getXloc(), monster.getYLoc(), null);
+
+
+
+
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        monster.moveRight();
-        repaint();
-        monster.moveLeft();
-        repaint();
+
+
+
+        //repaint();
     }
 }
