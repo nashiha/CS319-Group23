253a0901717b41109c036027281042277327990d
nashiha <nashiha.ahmed@yahoo.com>
2016-12-24 20:56:23 +0300

    Frame name corrected

--- ROM/View/SettingsPanel.java
+++ ROM/View/SettingsPanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SettingsPanel extends JPanel {
 
@@ -16,6 +18,8 @@ public class SettingsPanel extends JPanel {
 	private JButton back;
 	private JTextArea textArea1;
 	private JTextArea textArea2;
+	private String difficultyMode;
+	private String soundMode;
 
 	public JButton getBack() {
 		return back;
@@ -23,8 +27,49 @@ public class SettingsPanel extends JPanel {
 	public SettingsPanel() {
 		background = new ImageIcon(this.getClass().getResource("images/Menu_main_no_buttons.png")).getImage();
 		add(innerPanel);
-	}
+		easy.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (easy.isSelected())
+					difficultyMode = "easy";
+			}
+		});
+		medium.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (medium.isSelected())
+					difficultyMode = "medium";
+			}
+		});
+		hard.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (hard.isSelected())
+					difficultyMode = "hard";
+			}
+		});
 
+		on.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (on.isSelected())
+					soundMode = "on";
+			}
+		});
+		off.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (off.isSelected())
+					soundMode = "off";
+			}
+		});
+	}
+	public String getDifficultyMode(){
+		return difficultyMode;
+	}
+	public String getSoundMode(){
+		return soundMode;
+	}
 	@Override
 	public void paintComponent(Graphics g) {
 		g.drawImage(background, 0, 0, null);
