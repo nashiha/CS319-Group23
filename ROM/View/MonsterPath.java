87b28dacf2504eeb31210435ca503a67a6b57b46
efeUlasAkay <efeulasakayseyitoglu@gmail.com>
2016-12-24 03:55:12 +0200

    Monster path Updated!

--- ROM/View/MonsterPath.java
+++ ROM/View/MonsterPath.java
@@ -10,7 +10,6 @@ import java.awt.event.ActionListener;
 import java.util.Timer;
 import java.util.*;
 
-
 public class MonsterPath extends JPanel implements ActionListener {
     Image game_background;
     private Timer timer;
@@ -20,25 +19,103 @@ public class MonsterPath extends JPanel implements ActionListener {
     private int startCheck;
     private TimerTask task;
     private int currentCheck = 0;
+    private boolean isTower1Ready;
+    private boolean isTower2Ready;
+    private boolean isTower3Ready;
+    private boolean isTower4Ready;
+    JPanel innerPanel;
+    private JButton tower1;
+    private JButton tower2;
+    private JButton tower3;
+    private JButton tower4;
 
 
     public MonsterPath() {
 
         game_background = new ImageIcon(this.getClass().getResource("/game_background.png")).getImage();
 
+        //tower set to false initially
+        isTower1Ready = false;
+        isTower2Ready = false;
+        isTower3Ready = false;
+        isTower4Ready = false;
+
+        game_background = new ImageIcon(this.getClass().getResource("images/game_background.png")).getImage();
+        tower1 = new JButton("add tower");
+        tower3 = new JButton("add tower");
+        tower2 = new JButton("add tower");
+        tower4 = new JButton("add tower");
+        setLayout(null);
+        tower1.setBounds(100,200,100,20);
+        tower2.setBounds(275,300,100,20);
+        tower3.setBounds(300,400,100,20);
+        tower4.setBounds(270,200,100,20);
+
+
+        //tower1 action listener
+        tower1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == tower1) {
+                    isTower1Ready = true;
+                    repaint();
+                }
+            }
+        });
+        add(tower1);
+
+        //tower2 action listener
+        tower2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == tower2) {
+                    isTower2Ready = true;
+                    repaint();
+                }
+
+            }
+        });
+        add(tower2);
+
+
+        //tower3action listener
+        tower3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == tower3) {
+                    isTower3Ready = true;
+                    repaint();
+                }
+            }
+        });
+        add(tower3);
+
+       tower4.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               if(e.getSource() == tower4) {
+                   isTower4Ready = true;
+                   repaint();
+               }
+           }
+       });
+        add(tower4);
+
+
+//        add(innerPanel);
         pathCheck = 0;
         startCheck = 0;
         monsterPanel = new MonsterPanel();
         monster = monsterPanel.getMonster();
-        JOptionPane.showMessageDialog(null,"you have 30 seconds to set up towers before the game begins!");
+        //JOptionPane.showMessageDialog(null,"you have 30 seconds to set up towers before the game begins!");
 
         timer = new Timer();
         task = new TimerTask() {
             @Override
             public void run() {
-                if(startCheck < 30)
+              /*  if(startCheck < 30)
                     startCheck++;
-                else {
+                else {*/
                     if(currentCheck < 135)
                         monster.moveRight(1);
                     else if(currentCheck < 355 )
@@ -57,7 +134,8 @@ public class MonsterPath extends JPanel implements ActionListener {
                         JOptionPane.showMessageDialog(null,"Model.Monster Reached the castle!!");
                     currentCheck++;
                     repaint();
-                }
+
+              //  }
 
             }
         };
@@ -67,7 +145,7 @@ public class MonsterPath extends JPanel implements ActionListener {
     }
 
     public void move() {
-        timer.scheduleAtFixedRate(task,100,100);
+        timer.scheduleAtFixedRate(task,50,50);
     }
 
 
@@ -79,7 +157,26 @@ public class MonsterPath extends JPanel implements ActionListener {
 
         g.drawImage(monsterPanel.returnMonsterImage(), monster.getXloc(), monster.getYLoc(), null);
 
+        if(isTower1Ready) {
+            Image image1 = new ImageIcon(this.getClass().getResource("images/play.png")).getImage();
+            g.drawImage(image1,30,100,null);
 
+        }
+
+        if(isTower2Ready) {
+            Image image1 = new ImageIcon(this.getClass().getResource("images/play.png")).getImage();
+            g.drawImage(image1,200,200,null);
+        }
+
+        if(isTower3Ready) {
+            Image image1 = new ImageIcon(this.getClass().getResource("images/play.png")).getImage();
+            g.drawImage(image1,310,280,null);
+        }
+
+        if(isTower4Ready) {
+            Image image1 = new ImageIcon(this.getClass().getResource("images/play.png")).getImage();
+            g.drawImage(image1,320,100,null);
+        }
 
 
 
@@ -87,8 +184,6 @@ public class MonsterPath extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-
 
         //repaint();
     }
